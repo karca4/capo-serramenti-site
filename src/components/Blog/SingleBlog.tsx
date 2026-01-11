a988c41af63b0697fe3a2181e305b9a32d0eb6ce
karca4 <carmine.pc94@yahoo.it>
2026-01-11 18:46:52 +0100

    update blog and menu data with new titles, descriptions, and optional author fields

--- src/components/Blog/SingleBlog.tsx
+++ src/components/Blog/SingleBlog.tsx
@@ -29,22 +29,24 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
             {paragraph}
           </p>
           <div className="flex items-center">
-            <div className="border-body-color/10 mr-5 flex items-center border-r pr-5 xl:mr-3 xl:pr-3 2xl:mr-5 2xl:pr-5 dark:border-white/10">
-              <div className="mr-4">
-                <div className="relative h-10 w-10 overflow-hidden rounded-full">
-                  <Image src={author.image} alt="author" fill />
+            {author && (
+              <div className="border-body-color/10 mr-5 flex items-center border-r pr-5 xl:mr-3 xl:pr-3 2xl:mr-5 2xl:pr-5 dark:border-white/10">
+                <div className="mr-4">
+                  <div className="relative h-10 w-10 overflow-hidden rounded-full">
+                    <Image src={author.image} alt="author" fill />
+                  </div>
                 </div>
-              </div>
-              <div className="w-full">
-                <h4 className="text-dark mb-1 text-sm font-medium dark:text-white">
-                  By {author.name}
-                </h4>
-                <p className="text-body-color text-xs">{author.designation}</p>
-              </div>
-            </div>
+                <div className="w-full">
+                  <h4 className="text-dark mb-1 text-sm font-medium dark:text-white">
+                    By {author.name}
+                  </h4>
+                  <p className="text-body-color text-xs">{author.designation}</p>
+                </div>
+              </div>)
+            }
             <div className="inline-block">
               <h4 className="text-dark mb-1 text-sm font-medium dark:text-white">
-                Date
+                Pubblicato il
               </h4>
               <p className="text-body-color text-xs">{publishDate}</p>
             </div>
--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -1,47 +1,14 @@
-import { Blog } from "@/types/blog";
+import {Blog} from "@/types/blog";
 
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "Le nuove soluzioni per le zanzariere",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Non farti attaccare dalle zanzare quest'estate! Scopri le nostre innovative zanzariere su misura, progettate per garantire protezione ed eleganza alla tua casa.",
     image: "/images/blog/blog-01.jpg",
-    author: {
-      name: "Samuyl Joshi",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
     tags: ["creative"],
     publishDate: "2025",
-  },
-  {
-    id: 2,
-    title: "9 simple ways to improve your design skills",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-02.jpg",
-    author: {
-      name: "Musharof Chy",
-      image: "/images/blog/author-02.png",
-      designation: "Content Writer",
-    },
-    tags: ["computer"],
-    publishDate: "2025",
-  },
-  {
-    id: 3,
-    title: "Tips to quickly improve your coding speed.",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
-    image: "/images/blog/blog-03.jpg",
-    author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
-    tags: ["design"],
-    publishDate: "2025",
-  },
+  }
 ];
 export default blogData;
